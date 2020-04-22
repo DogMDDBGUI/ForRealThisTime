56f0853c0c679729cc26ff9db72b63cbd118f6e2
cuom1999 <lephuocdinh99@gmail.com>
2020-04-22 18:44:49 -0500

    connect backend and front end

--- backend/node/App/model/dbConn.js
+++ backend/node/App/model/dbConn.js
@@ -1,18 +1,30 @@
 'use strict';
 var mysql = require('mysql');
 
+const config = { // set up config
+    host : 'localhost',
+    user : 'root',
+    port : '3306',
+    database: 'dogmd',
+    password: 'password',
+    multipleStatements: true, // allow multiple statement queries
+}// config 
+
 // Local mysql db connection
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'dogmd'
-});
+var connection = mysql.createConnection(config);
+
+connection.connect(function(err) { // check if connected
+    if (err) {
+        throw err;
+        console.log('Cannot connect to database.');
+    }
+    else console.log('Connected to database.')
+}) // check connection
 
 // Error handling
-connection.connect(function(err) {
-  if (err) throw err;
-});
+// connection.connect(function(err) {
+//   if (err) throw err;
+// });
 
 // Export SQL conn
 module.exports = connection;
---
+++ backend/node/App/routes.js
@@ -0,0 +1,4 @@
+module.exports = function(app) {
+    const loginRoute = require('./endpoints/login');
+    app.use('/api/login', loginRoute);   
+}
