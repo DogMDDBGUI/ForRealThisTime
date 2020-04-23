5ce4ecb5b72a5a08507bfd3ab8b85248d162c1fb
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-04-23 15:58:58 -0500

    Added dog post

---
+++ backend/node/App/endpoints/dogs.js
@@ -0,0 +1,35 @@
+const connection = require('../model/dbConn') // connected db 
+const express = require('express')
+const router = express.Router() // express route handler 
+
+
+router.post('/dogs', (req, res) => { // receive event data from the frontend
+    let newDog = req.body;
+    console.log(newDog);
+    if (!newDog.id || 
+        !newDog.breed_id || 
+        !newDog.owner_id || 
+        !newDog.name || 
+        !newDog.age || 
+        !newDog.gender ||
+        !newDog.conditions ||
+        !newDog.imageURL) {
+        res.status(400).json({
+          "code": 400,
+          "response":"Please provide input for all fields."
+        });
+        return;
+    }
+
+    connection.query(
+        `INSERT INTO users (id, breed_id, owner_id, name, age, gender, conditions, imageURL)\
+                        VALUES ('${newDog.id}', '${newDog.breed_id}', '${newDog.owner_id}', '${newDog.name}', '${newDog.age}', 
+                                '${newDog.gender}', '${newDog.conditions}', '${newDog.imageURL}');`,
+        (err, rows, fields) => {
+            if (err) throw err
+            res.json({"code": "200", "msg": "Register Successfully"});
+        }
+    );
+}) 
+
+module.exports = router 
