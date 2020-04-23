622c4850a5b9d3b894ade0ab61ad4de4911f3004
Nicholas <nylu@smu.edu>
2020-04-22 23:04:15 -0500

    Adding login

--- backend/node/App/endpoints/login.js
+++ backend/node/App/endpoints/login.js
@@ -3,15 +3,61 @@ const express = require('express')
 const router = express.Router() // express route handler 
 
 // login
+// router.post('/', (req, res) => {
+//     connection.query(
+//         `SELECT * FROM veterinarian;`, 
+//         (err, rows, fields)=>{
+//         if(err) throw err
+//         res.end(JSON.stringify(rows))
+//     })
+// })
+
 router.post('/', (req, res) => {
-    connection.query(
-        `SELECT * FROM veterinarian;`, 
-        (err, rows, fields)=>{
-        if(err) throw err
-        res.end(JSON.stringify(rows))
-    })
+    let user = req.body
+    var query = `SELECT * FROM users WHERE email = ?;`
+    
+    connection.query(query, [user.email], function(err, res) {
+        
+    }
+    
+    res.end 
 })
 
+// Attempts to login user if found in database
+// User.loginUser = function(login_user, result){
+//     sql.query('SELECT * FROM users WHERE email = ?;',[login_user.email], function(err, res){
+//       if(err) {
+//         result({"code":400,"response":"Fatal SQL error ocurred"}, null);
+//       }
+//       else {
+//         //if email was found
+//         if(res.length > 0) {
+//           //if passwords match
+//           if(res[0].password == login_user.password){
+//             result(null,{
+//               "code":200,
+//               "response":"Login was sucessfull.",
+//               "id":res[0].id,
+//               "email":res[0].email,
+//               "first_name":res[0].first_name,
+//               "last_name":res[0].last_name,
+//               "password":res[0].password,
+//               "role_id":res[0].role_id,
+//               "zipcode":res[0].zipcode
+//               });
+//           }
+//           //if passwords are different
+//           else{
+//             result({"code":204,"response":"Email and/or password do not match."},null);
+//           }
+//         }
+//         //if no email id found
+//         else{
+//           result({"code":204,"response":"Email does not exist."},null);
+//         }
+//       }
+//     });
+//   }
 
 router.post('/register', (req, res) => { // receive event data from the frontend
     let newUser = req.body;
