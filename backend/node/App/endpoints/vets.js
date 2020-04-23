88e6a2e47e84c4840a69e1c7fc655a4d34291b90
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-04-23 17:28:43 -0500

    added vet puts

--- backend/node/App/endpoints/vets.js
+++ backend/node/App/endpoints/vets.js
@@ -28,4 +28,68 @@ router.post('/users/vet', (req, res) => { // receive event data from the fronten
     );
 }) 
 
+/*
+router.put('/users/vet/area_id:user_id', async (req, res) => {
+
+    let user_id = req.param('user_id');
+    let area_id = req.param('area_id');
+
+    connection.query(
+        `UPDATE veterinarian (area_id)\
+                        SET ('${user_id.area_id}');`,
+        (err, rows, fields) => {
+            if (err) throw err
+            res.json({"code": "200", "msg": "Register Successfully"});
+        }
+    );
+});
+
+
+router.put('/users/vet/skills:user_id', async (req, res) => {
+
+    let user_id = req.param('user_id');
+    let skills = req.param('skills');
+
+    connection.query(
+        `UPDATE veterinarian (skills)\
+                        SET ('${user_id.skills}');`,
+        (err, rows, fields) => {
+            if (err) throw err
+            res.json({"code": "200", "msg": "Register Successfully"});
+        }
+    );
+});
+
+
+router.put('/users/vet/years_experience:user_id', async (req, res) => {
+
+    let user_id = req.param('user_id');
+    let years_experience = req.param('years_experience');
+
+    connection.query(
+        `UPDATE veterinarian (years_experience)\
+                        SET ('${user_id.years_experience}');`,
+        (err, rows, fields) => {
+            if (err) throw err
+            res.json({"code": "200", "msg": "Register Successfully"});
+        }
+    );
+});
+
+
+router.put('/users/vet/ratings:user_id', async (req, res) => {
+
+    let user_id = req.param('user_id');
+    let ratings = req.param('ratings');
+
+    connection.query(
+        `UPDATE veterinarian (ratings)\
+                        SET ('${user_id.ratings}');`,
+        (err, rows, fields) => {
+            if (err) throw err
+            res.json({"code": "200", "msg": "Register Successfully"});
+        }
+    );
+});
+*/
 module.exports = router 
