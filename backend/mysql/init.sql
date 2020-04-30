40d58234ccdc5f98de22629d8af5944b4a501228
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-04-30 16:34:41 -0500

    finished

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -116,31 +116,3 @@ INSERT INTO appointment(id, time, date, dog_id, status, vet_id) VALUES
 (1, '10:00:00', '2020-05-13', 1, 'BOOKED', 1),
 (2, '15:00:00', '2020-05-07', 2, 'CANCELLED', 6),
 (3, '08:30:00', '2020-06-13', 4, 'BOOKED', 6);
-
-
-/*ALTER USER 'root'@'localhost' IDENTIFIED BY ''; */
-
-/*
--- create table in DB
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
-);
-
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
-
--- create user called `manager` with password `Password`
-CREATE USER 'manager'@'%' IDENTIFIED BY 'Password';
-
--- give access to manager on db
-GRANT ALL PRIVILEGES ON db.* TO 'manager'@'%';
-
--- set password method to native password for mysql workbench access (mysql 8 issue)
-ALTER USER 'manager'@'%' IDENTIFIED WITH MYSQL_NATIVE_PASSWORD BY 'Password';
-
--- flush them privileges
-FLUSH PRIVILEGES;
-*/
